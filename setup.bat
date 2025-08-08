01602d6556391c73aba4cd2e5301e42fe65681a6
McSnurtle <joepro17@outlook.com>
2025-08-08 13:26:08 -0400

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -4,8 +4,6 @@ echo "Running installation of https://github.com/McSnurtle/voip.git"
 
 python -m venv venv
 
-.\venv\Scripts\activate
+call .\venv\Scripts\activate
 
 pip install --upgrade -r requirements.txt
-
-python src/client.py
