fa5f28a1b9a9bf74ad84b8d628112eea04f029bd
McSnurtle <joepro17@outlook.com>
2025-08-14 11:23:23 -0400

    Setup scripts no longer care where they're run from

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,6 +2,10 @@
 
 echo "Running installation for https://github.com/McSnurtle/voip.git"
 
+# installation is pwd-sensitive, so just to be sure:
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd "$SCRIPT_DIR/.." || exit 1
+
 python -m venv venv
 
 source ./venv/bin/activate
